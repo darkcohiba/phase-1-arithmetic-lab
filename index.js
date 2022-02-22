4afb2a3406e6a0b428e61fb0ecb113c9058a9e57
Sam <samuel.waters@gmail.com>
2022-02-22 16:58:10 -0700

    commit tree c289536793e6829fcbb8f4303721509fb10c2b65

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function multiply(num1, num2){
-    console.log(num1 * num2);
-}
-function random(){
-    let random = ((Math.random() * 10) + 1);
-    console.log(Math.round(random));
-}
-function mod(num3, num4){
-    console.log(num3 % num4);
-}
-function max(x){
-    return(Math.max(x));
-}
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+let x = (1, 2, 20);
+let max = Math.max(x)
+
+
