195f3d06a99708860816278f95b4afa8cc210e60
Sam <samuel.waters@gmail.com>
2022-02-22 16:52:24 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+function multiply(num1, num2){
+    console.log(num1 * num2);
+}
+function random(){
+    let random = ((Math.random() * 10) + 1);
+    console.log(Math.round(random));
+}
+function mod(num3, num4){
+    console.log(num3 % num4);
+}
+function max(x){
+    return(Math.max(x));
+}
